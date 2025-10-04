7bd2e7ae0e0463fbf8246cc3724a5fd946a35af4
Francis261 <francisdominic986532@gmail.com>
2025-10-04 23:24:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const ministun = require('ministun');
+const Ministun = require('ministun');
 
-const server = ministun.createServer();
+const config = {
+  udp4: true,
+  udp6: true,
+  port: process.env.PORT || 3000,
+  log: console.log,
+  err: console.error,
+  sw: true
+};
 
-server.on('bindingRequest', (req, res) => {
-  res.send(req);
-});
+const server = new Ministun(config);
 
-server.listen(3000, () => {
-  console.log('STUN server listening on port 3000');
-});
+async function startServer() {
+  try {
+    await server.start();
+    console.log(`STUN server listening on port ${config.port}`);
+  } catch (err) {
+    console.error('Failed to start STUN server:', err);
+  }
+}
+
+startServer();
